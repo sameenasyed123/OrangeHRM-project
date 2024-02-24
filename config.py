245d1cc8ab81af737b5c7ac61bdf47be8cb97cfa
sameena <sameenasyed108@gmail.com>
2024-02-24 12:28:42 +0530

    some changes happen in files and change structure of project implementation of pytest

--- config.py
+++
@@ -1,4 +0,0 @@
-# utils/config.py
-class Config:
-    BASE_URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-    CHROME_DRIVER_PATH = "C:\OrangeHRM project\chromedriver-win64\chromedriver.exe"  # Update with the actual path
